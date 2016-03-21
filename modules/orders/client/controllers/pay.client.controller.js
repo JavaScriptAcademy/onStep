c5131bf9d7eec9163153e555abd6ad18f03a8bc5
Clairejing <wenjing.liu09@foxmail.com>
2016-03-21 10:08:59 +0800

    Fix some space cause bugs

--- modules/orders/client/controllers/pay.client.controller.js
+++ modules/orders/client/controllers/pay.client.controller.js
@@ -12,16 +12,16 @@
     $scope.payMethods = [];
     $scope.getPayMethod = function(){
       var methods = [
-      {
-        name: 'AliPay',
-        image: 'http://xianse.cc/upload/201410/1108204814.350x350.jpg',
-        link: 'https://www.alipay.com/'
-      },
-      {
-        name: 'WeChatPay',
-        image: 'http://reso2.yiihuu.com/1288862-z.jpg',
-        link: 'https://pay.weixin.qq.com/index.php/home/login?return_url=/'
-      }
+        {
+          name: 'AliPay',
+          image: 'http://xianse.cc/upload/201410/1108204814.350x350.jpg',
+          link: 'https://www.alipay.com/'
+        },
+        {
+          name: 'WeChatPay',
+          image: 'http://reso2.yiihuu.com/1288862-z.jpg',
+          link: 'https://pay.weixin.qq.com/index.php/home/login?return_url=/'
+        }
       ];
       $scope.payMethods = methods;
     };
