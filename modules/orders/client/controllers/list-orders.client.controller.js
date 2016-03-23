0c03f85438ff5973fa63d86ae96e357ac40de89b
freshmanyuan <freshmanyuan@163.com>
2016-03-23 13:50:04 +0800

    remove ng-material

--- modules/orders/client/controllers/list-orders.client.controller.js
+++ modules/orders/client/controllers/list-orders.client.controller.js
@@ -5,9 +5,9 @@
     .module('orders')
     .controller('OrdersListController', OrdersListController);
 
-  OrdersListController.$inject = ['OrdersService', 'Authentication','ngMaterial'];
+  OrdersListController.$inject = ['OrdersService', 'Authentication'];
 
-  function OrdersListController(OrdersService, Authentication, ngMaterial) {
+  function OrdersListController(OrdersService, Authentication) {
     var vm = this;
 
     vm.orders = OrdersService.query();
